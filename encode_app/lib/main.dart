48c209d68556ef2213993a4eebdcbbb8bbc0973e
DevPhiCap <phi.np.1953@aptechlearning.edu.vn>
2024-09-01 15:24:43 +0700

    add xor key

--- encode_app/lib/main.dart
+++ encode_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
 import 'dart:typed_data';
@@ -41,6 +43,8 @@ class _MyHomePageState extends State<MyHomePage> {
   var encrypted;
 
   String encryptedText = '';
+  String xorText = '';
+  List<int> keyXor = [];
 
   @override
   void initState() {
@@ -70,6 +74,62 @@ class _MyHomePageState extends State<MyHomePage> {
   Uint8List _removePadding(Uint8List paddedData) {
     int pad = paddedData.last;
     return Uint8List.sublistView(paddedData, 0, paddedData.length - pad);
+  }
+
+  String xorStrings(String str1, String str2, String str3) {
+    const String base64Chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    List<int> result = [];
+    List<int> key = [];
+
+    for (int i = 0; i < str1.length; i++) {
+      if (str1[i] != '=') {
+        int xorValue = str1.codeUnitAt(i % str1.length) ^
+            str2.codeUnitAt(i % str2.length) ^
+            str3.codeUnitAt(i % str3.length);
+
+        int adjustedValue = xorValue % 64;
+        result.add(base64Chars.codeUnitAt(adjustedValue));
+        key.add((xorValue / 64).floor());
+      } else {
+        result.add('='.codeUnitAt(0));
+      }
+    }
+
+    keyXor = key;
+    return String.fromCharCodes(result);
+  }
+
+  String decodeXorString(String encodedString, String str2, String str3) {
+    const String base64Chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    List<int> result = [];
+
+    for (int i = 0; i < encodedString.length; i++) {
+      if (encodedString[i] != '=') {
+        int originUnit = base64Chars.indexOf(encodedString[i]) + 64 * keyXor[i];
+        // Reverse the XOR operation
+        int originalValue = originUnit ^
+            str2.codeUnitAt(i % str2.length) ^
+            str3.codeUnitAt(i % str3.length);
+
+        result.add(originalValue);
+      } else {
+        result.add('='.codeUnitAt(0));
+      }
+    }
+
+    return String.fromCharCodes(result);
+  }
+
+  String encryptText(String text) {
+    Uint8List plaintext = Uint8List.fromList(text.codeUnits);
+
+    Uint8List paddedPlaintext = _addPadding(plaintext);
+
+    final encrypted = encrypter.encryptBytes(paddedPlaintext, iv: iv);
+
+    return encrypted.base64;
   }
 
   @override
@@ -119,16 +179,29 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                 if (textEditingController.text.isEmpty) const Text(''),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text('kết quả bộ 3:'),
+                Text(xorText),
                 ElevatedButton(
                   onPressed: () {
-                    Uint8List plaintext = Uint8List.fromList(
-                        textEditingController.text.codeUnits);
-                    Uint8List paddedPlaintext = _addPadding(plaintext);
+                    String str1 = textEditingController.text;
+                    String str2 = str1.substring((str1.length / 3).ceil()) +
+                        str1.substring(0, (str1.length / 3).floor());
+                    String str3 = str2.substring((str2.length / 3).ceil()) +
+                        str2.substring(0, (str2.length / 3).floor());
 
-                    encrypted = encrypter.encryptBytes(paddedPlaintext, iv: iv);
+                    str1 = encryptText(str1);
+                    str2 = encryptText(str2);
+                    str3 = encryptText(str3);
+
                     setState(() {
-                      encryptedText = encrypted.base64;
+                      xorText = xorStrings(str1, str2, str3);
+                      encryptedText = str1;
                     });
+
+                    print("xorrrrr: ${decodeXorString(xorText, str2, str3)}");
 
                     _saveData(textEditingController.text, encryptedText);
                   },
